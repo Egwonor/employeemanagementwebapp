d7537356ee79f1041f9c82dc61a8ea858f18ea7a
oghen <oghen@DESKTOP-SUABB0G>
2020-05-27 11:57:54 +0200

    first commit

---
+++ employeeWebApp/src/test/java/com/springboot/emloyee/webapp/employeeWebApp/EmployeeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.emloyee.webapp.employeeWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
